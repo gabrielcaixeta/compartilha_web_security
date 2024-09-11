7c4ea209fa449a91137a217de4e0e6e86d369705
Gabriel Caixeta Silva <gabriel@soft4.com.br>
2024-09-11 18:20:36 -0300

    udpate import user scritp

--- documentacao/teste.sql
+++ documentacao/teste.sql
@@ -1,2 +1,2 @@
-insert usuario (nome,  login , senha) values ('João da silva', 'joao', md5(123))
-insert usuario (nome,  login , senha) values ('Maria Carlota Joaquina', 'maria', md5(123))
+insert usuario (nome,  login , senha) values ('João da silva', 'joao', md5(123));
+insert usuario (nome,  login , senha) values ('Maria Carlota Joaquina', 'maria', md5(123));
